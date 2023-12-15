721e09ed58188083b1ca0a7132a2e613badcd6fd
Adekunle Adedeji <ceeepath@gmail.com>
2023-12-15 15:49:00 +0000

    added security groups for Database and EFS

--- modules/3-Tier-VPC/locals.tf
+++ modules/3-Tier-VPC/locals.tf
@@ -50,6 +50,30 @@ locals {
     }
   ]
 
+  db_ingress_rules = [
+    {
+      from_port       = 3306
+      to_port         = 3306
+      protocol        = "tcp"
+      security_groups = [aws_security_group.web.id]
+    }
+  ]
+
+  efs_ingress_rules = [
+    {
+      from_port       = 2049
+      to_port         = 2049
+      protocol        = "tcp"
+      security_groups = [aws_security_group.web.id]
+    },
+    {
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      security_groups = [aws_security_group.ssh.id]
+    }
+  ]
+
   egress_rule = [
     {
       from_port   = 0
